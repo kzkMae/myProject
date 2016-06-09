66b57d39850dc96ec229084d93014510f3345a37
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-06-09 15:01:34 +0900

    エラーチャックに特化したDeadEndファイルを作成
    0でなかったら強制終了する関数のみを実装

---
+++ code/Check/DeadEnd.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+import sys
+#このプログラムを強制的に終了させるための関数様々な状況下で実行可能にしておく
+
+def deadErrorEnd(checkNumber):
+    if not (checkNumber == 0):
+        print '終了します'
+        print checkNumber
+        sys.exit()
+    return 0
