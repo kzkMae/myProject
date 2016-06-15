84dea423327fbb8acf375d50784b7acc0858e9ff
kzkMae <16jkm23@ms.dendai.ac.jp>
2016-06-15 18:30:54 +0900

    パケットキャプチャー終了関数作成

--- code/vmGenymotion/apkGenymotion/packetCapture.py
+++ code/vmGenymotion/apkGenymotion/packetCapture.py
@@ -11,6 +11,8 @@ from Check.DeadEnd import *
 timesec = [2,0.2,0.5]
 cmdCapS = ' shell am start -n jp.co.taosoftware.android.packetcapture/.PacketCaptureActivity'
 keyevent = ' shell input keyevent '
+swipe = ' shell input swipe '
+tap = ' shell input tap '
 
 #パケットキャプチャー開始
 #インプットを取り付ける
@@ -37,12 +39,50 @@ def packetCaptureStart(adbFile):
     checkNum += os.system(cmdE)
     time.sleep(timesec[2])
 
+    cmd = adbFile + keyevent + 'KEYCODE_HOME'
+    checkNum += os.system(cmd)
+    time.sleep(timesec[0])
     return checkNum
 
 #パケットキャプチャーソフト起動のメイン関数
 def packetCaptureStartMain(adbExeFile):
     #パケットキャプチャー起動
     checkNum = packetCaptureStart(adbExeFile)
+    #ErrorCheck
+    deadErrorEnd(checkNum)
+    time.sleep(timesec[0])
+    return checkNum
+
+
+def packetCaptureEnd(adbFile):
+    checkNum =0
+    cmd = adbFile + keyevent + 'KEYCODE_HOME'
+    checkNum += os.system(cmd)
+    time.sleep(timesec[0])
+    #スワイプ
+    cmd = adbFile + swipe + '10 20 10 1000'
+    checkNum += os.system(cmd)
+    time.sleep(timesec[2])
+    #タップ
+    cmd = adbFile + tap + '300 150'
+    checkNum += os.system(cmd)
+    time.sleep(timesec[2])
+    #キャプチャー停止
+    cmd = adbFile + keyevent + 'KEYCODE_DPAD_RIGHT'
+    checkNum += os.system(cmd)
+    time.sleep(timesec[1])
+    checkNum += os.system(cmd)
+    time.sleep(timesec[1])
+    cmd = adbFile + keyevent + 'KEYCODE_ENTER'
+    checkNum += os.system(cmd)
+    time.sleep(timesec[1])
+    return checkNum
+
+
+def packetCaptureEndMain(adbExeFile):
+    checkNum = 0
+    #パケットキャプチャー終了
+    checkNum = packetCaptureEnd(adbExeFile)
     #ErrorCheck
     deadErrorEnd(checkNum)
     time.sleep(timesec[0])
